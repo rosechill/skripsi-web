86aed0bdc99e4f3b6072e710ff17ce9160d79b18
bagas <bagasab11@gmail.com>
2024-04-20 15:26:36 +0700

    add smooth scroll

--- src/components/organisms/ContainerProvider/index.tsx
+++ src/components/organisms/ContainerProvider/index.tsx
@@ -1,13 +1,18 @@
-"use client"
-import * as React from 'react';
+"use client";
+import * as React from "react";
 import { NextUIProvider } from "@nextui-org/react";
-
+import { useEffect } from "react";
 export default function ContainerProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <NextUIProvider>{children}</NextUIProvider>
-  );
+  useEffect(() => {
+    (async () => {
+      // @ts-ignore
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
